652314709997c36113a3108994c324bb403d2665
rahul pareek <itsrahul615@gmail.com>
2025-10-16 15:21:32 +0530

    Used LinkedHashSet subClass of HashSet

---
+++ CollectionFramework/src/Set/LinkedHashSetDemo.java
@@ -0,0 +1,20 @@
+package Set;
+import java.util.*;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		//LHS follows insertion order while HashSet sort it
+		LinkedHashSet<String> lhs = new LinkedHashSet<>();
+		lhs.add("A");
+		lhs.add("C");
+		lhs.add("E");
+		lhs.add("B");
+		lhs.add("D");
+		lhs.add(null);
+		lhs.add(null);//allow one null
+		lhs.add("D");
+		System.out.println(lhs);
+	}
+
+}
